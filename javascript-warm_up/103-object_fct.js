9229f9e09d5b5e52673bfe1af32a2edf98c60509
HMDT2023 <merveilhouenagnon@gmail.com>
2023-09-06 10:54:32 +0100

    103

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -5,17 +5,12 @@ const myObject = {
 };
 console.log(myObject);
 
-function incr () {
-  this.value++;
-}
+
 
 // Utilisez bind pour lier la fonction incr à myObject
-myObject.incr = incr.bind(myObject);
-
-// Redéfinissez la méthode toString pour supprimer la mention "[Function: bound incr]"
-myObject.incr.toString = function () {
-  return 'function incr() { [native code] }';
-};
+myObject.incr = function () {
+  this.value++;
+}
 
 myObject.incr();
 console.log(myObject);
