e3d430f03267313fd0e4aaa5e92993888c4e461c
houenagnon <merveilhouenagnon@gmail.com>
2023-09-14 20:37:24 +0100

    3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,31 +1,5 @@
-const people = [
-    {
-      name: "Alice",
-      age: 30,
-      skills: ["JavaScript", "HTML", "CSS"],
-    },
-    {
-      name: "Bob",
-      age: 25,
-      skills: ["Python", "C++", "Java"],
-    },
-    {
-      name: "Charlie",
-      age: 35,
-      skills: ["JavaScript", "Node.js", "React"],
-    },
-  ];
-  
-  // Utilisation de map pour obtenir une liste de compétences de toutes les personnes
-  const skillsList = people.map(person => person.skills);
-  
-  // Utilisation de flat pour aplatir la liste de compétences
-  const flattenedSkills = skillsList.flat();
+const request = require('request');
+const fs = require('fs');
 
-  
-  // Utilisation de filter pour filtrer les personnes ayant des compétences en JavaScript
-const javascriptDevelopers = people.filter(person =>
-    person.skills.includes("JavaScript")
-  );
-
-  console.log(javascriptDevelopers)
+request.get(process.argv[2])
+    .pipe(fs.createWriteStream(process.argv[3]));
